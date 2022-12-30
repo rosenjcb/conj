277adb0bf9c342443570dc71e17598b954646b3b
Jacob Rosenzweig <rosenjcb@gmail.com>
2022-12-29 21:35:50 -0800

    Removing all async code from redis db namespace

--- board-manager/src/board_manager/query/db/redis.clj
+++ board-manager/src/board_manager/query/db/redis.clj
@@ -3,18 +3,18 @@
    [taoensso.carmine :as car]))
 
 (defn get-keys [redis-conn pattern]
-  @(future (car/wcar redis-conn (car/keys pattern))))
+  (car/wcar redis-conn (car/keys pattern)))
 
 #_{:clj-kondo/ignore [:redefined-var]}
 (defn get [redis-conn key]
-  @(future (car/wcar redis-conn (car/get key))))
+  (car/wcar redis-conn (car/get key)))
 
 #_{:clj-kondo/ignore [:redefined-var]}
 (defn set [redis-conn key value]
-  (future (car/wcar redis-conn (car/set key value))))
+  (car/wcar redis-conn (car/set key value)))
 
 (defn del [redis-conn key]
-  (future (car/wcar redis-conn (car/del key))))
+  (car/wcar redis-conn (car/del key)))
 
 (defn flush-all [redis-conn]
-  (future (car/wcar redis-conn (car/flushall))))
+  (car/wcar redis-conn (car/flushall)))
