d494ca642322c04473e17ecafead7007232a3c4a
Jacob Rosenzweig <rosenjcb@gmail.com>
2023-02-10 18:55:00 -0800

    Small styling change

--- web-app/src/components/NavBar.jsx
+++ web-app/src/components/NavBar.jsx
@@ -141,7 +141,7 @@ export const WithNavBar = ({ component }) => {
 
 const FixedWidth = styled.div`
   margin: 0 auto;
-  height: calc(
+  min-height: calc(
     100vh - 40px - 8px - 2px
   ); //full height - fixed navbar height - fixed navbar padding - border)
   overflow-y: scroll;
